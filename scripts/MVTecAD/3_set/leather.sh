e8c9d5b5653604e1f9b58cc4a3f16693c3ed4267
Hiroki Kobayashi <56967584+koba-jon@users.noreply.github.com>
2023-04-28 21:18:58 +0900

    Add files via upload

--- scripts/MVTecAD/3_set/leather.sh
+++ scripts/MVTecAD/3_set/leather.sh
@@ -5,6 +5,6 @@ TARGET_DATASET='MVTecAD/leather'
 
 mkdir -p "checkpoints/${TARGET_DATASET}/stage2/models_as_source/"
 for str in ${SOURCE_DATASET[@]}; do
-    cp checkpoints/${str}/stage2/models/* checkpoints/${TARGET_DATASET}/stage2/models_for_target/
+    cp checkpoints/${str}/stage2/models/* checkpoints/${TARGET_DATASET}/stage2/models_as_source/
 done
 
